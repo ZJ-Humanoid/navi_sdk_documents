7510ebf0932f55c2e9f32a4bd52ca8eadeb0a03b
谢郑勋 <10184870+xie-zhengxun@user.noreply.gitee.com>
2025-08-27 21:37:44 +0800

    chore: fix actions install step and set base path4

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,44 @@ const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
 const isUserOrOrgSite = repositoryName.endsWith('.github.io');
 
 export default defineConfig({
+  title: 'Navi机器人SDK开发指南',
+  description: '这是浙江人形机器人Navi系列的SDK和编程指引文档站点。',
   base: '/navi_sdk_documents/',
+
+  themeConfig: {
+    sidebar: [
+      {
+        items: [
+          { text: '快速开始', link: '/' },
+          {
+            text: 'ROS API',
+            collapsible: true,
+            items: [
+              { text: '导图', link: '/markmap_ros_api' },
+              { text: '文档', link: '/zj_humanoid_ros_api' },
+            ]
+          },
+          {
+            text: 'Message Type',
+            collapsible: true,
+            items: [
+              { text: '导图', link: '/markmap_message_type' },
+              { text: '文档', link: '/zj_humanoid_types' },
+            ]
+          },
+        ]
+      }
+    ],
+
+    outline: { level: [2, 4], label: 'On this page' },
+    appearance: false
+  },
+
+  vite: {
+    optimizeDeps: {
+      include: ['markmap-lib', 'markmap-view']
+    }
+  }
 });
 
 
